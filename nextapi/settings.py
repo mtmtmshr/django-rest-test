84514b2b1e6f41ed02e9333e2dc6e88c9018ab4e
mtmtmshr <s17t274@stu.kagawa-u.ac.jp>
2021-08-27 12:06:33 +0900

    added hosturl

--- nextapi/settings.py
+++ nextapi/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['next-api-mtmt.herokuapp.com']
 
 
 # Application definition
